c0ec07f0952bc2f6a52d2482e138d997af7a2f4c
Ryan2301010356 <denfalperbatasan03@gmail.com>
2025-03-25 21:51:05 +0800

    Keterangan commit

--- Pertemuan401/src/pertemuan401/Pertemuan401.java
+++ Pertemuan401/src/pertemuan401/Pertemuan401.java
@@ -4,6 +4,8 @@
  */
 package pertemuan401;
 
+import java.util.Scanner;
+
 /**
  *
  * @author LAB F
@@ -14,7 +16,23 @@ public class Pertemuan401 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner inp = new Scanner(System.in);
+        String[] nama = new String[5];
+        
+        for(int y=0;y<5;y++){
+            System.out.print("isi Data Teman ");
+            nama [y] = inp.nextLine();
+        }
+        
+        for(int i= 0; i<nama.length;i++){
+        System.out.println("Data ke-"+ (i+1)+":"+nama[i]);
+        }
+        
+        for(String x : nama) {
+            System.out.println(x);                                                                                                                                                                                                                                                                                                                                                                                             
+        }
+        
+       
     }
     
 }
