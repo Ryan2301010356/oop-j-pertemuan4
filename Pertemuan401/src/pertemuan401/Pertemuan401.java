7d25f81543dff5406f229824c61b810db0508a05
Ryan2301010356 <denfalperbatasan03@gmail.com>
2025-03-25 20:29:35 +0800

    Keterangan commit

---
+++ Pertemuan401/src/pertemuan401/Pertemuan401.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pertemuan401;
+
+/**
+ *
+ * @author LAB F
+ */
+public class Pertemuan401 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
